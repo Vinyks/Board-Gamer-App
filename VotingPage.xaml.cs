6a1b489073c7bfa68ca73b24216e107b5627a23f
Vinyks <54633020+Vinyks@users.noreply.github.com>
2025-09-04 17:03:29 +0200

    Basic Board Game List with Add and Remove

--- VotingPage.xaml.cs
+++ VotingPage.xaml.cs
@@ -1,9 +1,55 @@
-namespace Board_Gamer_App;
+﻿namespace Board_Gamer_App;
 
 public partial class VotingPage : ContentPage
 {
-	public VotingPage()
-	{
-		InitializeComponent();
-	}
+    private readonly List<string> _BoardGames = new();
+
+    public VotingPage()
+    {
+        InitializeComponent();
+        GamesCollectionView.ItemsSource = _BoardGames;
+    }
+
+    private async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+        // Show prompt to get board game name
+        string gameName = await DisplayPromptAsync(
+            "Add Board Game",
+            "Enter the name of the board game:",
+            "Add",
+            "Cancel",
+            "Board game name...",
+            maxLength: 50,
+            keyboard: Keyboard.Text
+        );
+
+        // Check if user entered a name and didn't cancel
+        if (!string.IsNullOrWhiteSpace(gameName))
+        {
+            AddBoardGame(gameName);
+        }
+    }
+
+    private void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.CommandParameter is string game)
+        {
+            _BoardGames.Remove((string)button.CommandParameter);
+
+            RefreshList();
+        }
+    }
+
+    private void AddBoardGame(string gameName)
+    {
+        _BoardGames.Add(gameName);
+
+        RefreshList();
+    }
+
+    private void RefreshList()
+    {
+        GamesCollectionView.ItemsSource = null;
+        GamesCollectionView.ItemsSource = _BoardGames;
+    }
 }
