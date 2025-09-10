9202c6a606866ce4208fded5c7749271fac5de5c
Vinyks <54633020+Vinyks@users.noreply.github.com>
2025-09-10 02:55:37 +0200

    Food Page Cuisine Drag and Drop Swap Implemented

---
+++ Cuisine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board_Gamer_App
+{
+    class Cuisine
+    {
+        private string _Name;
+        private int _Rank;
+
+        public Cuisine(string name, int rank)
+        {
+            _Name = name;
+            _Rank = rank;
+        }
+
+        public string Name { get => _Name; set => _Name = value; }
+        public int Rank { get => _Rank; set => _Rank = value; }
+    }
+}
--- FoodPage.xaml.cs
+++ FoodPage.xaml.cs
@@ -1,9 +1,134 @@
+﻿using Microsoft.Maui.Animations;
+
 namespace Board_Gamer_App;
 
 public partial class FoodPage : ContentPage
 {
-	public FoodPage()
-	{
-		InitializeComponent();
-	}
+    private List<Cuisine> _Cuisines = new();
+    public FoodPage()
+    {
+        InitializeComponent();
+
+        _Cuisines = new List<Cuisine> {
+            new Cuisine("Turkisch", 0),
+            new Cuisine("Griechisch", 1),
+            new Cuisine("Italienisch", 2),
+            new Cuisine("Chinesisch", 3),
+            new Cuisine("Japanisch", 4),
+            new Cuisine("Deutsch", 5)
+        };
+
+        CuisineList.ItemsSource = _Cuisines;
+    }
+
+    private int _StartingIndex;
+    public void OnDragStarting(object sender, DragStartingEventArgs e)
+    {
+        if (GetHoveredIndex(e, out int index))
+        {
+            _StartingIndex = index;
+        }
+    }
+
+    public void OnDropCompleted(object sender, DropEventArgs e)
+    {
+
+    }
+
+    public void OnDragOver(object sender, DragEventArgs e)
+    {
+
+    }
+    public void OnDrop(object sender, DropEventArgs e)
+    {
+        if (GetHoveredIndex(e, out int targetIndex))
+        {
+            MoveItemToIndex(_StartingIndex, targetIndex);
+        }
+
+        RefreshList();
+    }
+
+    private void MoveItemToIndex(int movingIndex, int targetIndex)
+    {
+        if (targetIndex == movingIndex) return;
+
+        bool movingDown = targetIndex > movingIndex;
+
+        Cuisine tempCuisine = _Cuisines[movingIndex];
+
+        if (movingDown)
+        {
+            for (int i = movingIndex; i < targetIndex; i++)
+            {
+                _Cuisines[i] = _Cuisines[i + 1];
+            }
+        }
+        else
+        {
+            for (int i = movingIndex; i > targetIndex; i--)
+            {
+                _Cuisines[i] = _Cuisines[i - 1];
+            }
+        }
+        _Cuisines[targetIndex] = tempCuisine;
+
+
+        for (int i = 0; i < _Cuisines.Count(); i++)
+        {
+            _Cuisines[i].Rank = i;
+        }
+    }
+
+    private bool GetHoveredIndex(DragStartingEventArgs e, out int index)
+    {
+        Point? p = e.GetPosition(CuisineList);
+        if (p.HasValue)
+        {
+            index = (int)(p.Value.Y / (_BorderHeight + _BorderMargin.VerticalThickness));
+            return true;
+        }
+        index = -1;
+        return false;
+    }
+
+    private bool GetHoveredIndex(DragEventArgs e, out int index)
+    {
+        Point? p = e.GetPosition(CuisineList);
+        if (p.HasValue)
+        {
+            index = (int)(p.Value.Y / (_BorderHeight + _BorderMargin.VerticalThickness));
+            return true;
+        }
+        index = -1;
+        return false;
+    }
+    private bool GetHoveredIndex(DropEventArgs e, out int index)
+    {
+        Point? p = e.GetPosition(CuisineList);
+        if (p.HasValue)
+        {
+            index = (int)(p.Value.Y / (_BorderHeight + _BorderMargin.VerticalThickness));
+            return true;
+        }
+        index = -1;
+        return false;
+    }
+
+    private double _BorderHeight;
+    private Thickness _BorderMargin;
+    private void OnBorderSizeChanged(object sender, EventArgs e)
+    {
+        if (sender is Border element)
+        {
+            _BorderHeight = element.Height;
+
+            _BorderMargin = element.Margin;
+        }
+    }
+    private void RefreshList()
+    {
+        CuisineList.ItemsSource = null;
+        CuisineList.ItemsSource = _Cuisines;
+    }
 }
